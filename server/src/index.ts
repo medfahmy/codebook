951fd6daa4a8c76bd1239fb9aa02e2157aeabed2
medfahmy <medfahmy99@gmail.com>
2021-09-08 16:01:36 +0100

    express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,32 @@
 import express, { Request, Response } from "express";
+import { router } from "./router";
+import { config } from "./utils/config";
 import { log } from "./utils/log";
+import mongoose from "mongoose";
+
+const { port, host, mongoURI } = config;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (_: Request, res: Response) => {
-  res.send("hello");
-});
+// app.get("/", (_: Request, res: Response) => {
+//   res.send("hello");
+// });
 
-app.listen(4000, () => log.info("server running"));
+export const connect = () => {
+  return mongoose
+    .connect(mongoURI)
+    .then(() => log.info("database connected"))
+    .catch((err) => {
+      log.error("db error", err);
+      process.exit(1);
+    });
+};
+
+app.listen(port, host, () => {
+  log.info(`server running at http://${host}:${port}`);
+  connect();
+  router(app);
+});
