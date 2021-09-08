ac7f6c5c0df4757c8c3f98a05f83c790c6be9662
medfahmy <medfahmy99@gmail.com>
2021-09-08 16:02:10 +0100

    express server

---
+++ server/src/router.ts
@@ -0,0 +1,4 @@
+import { Express } from "express";
+export const router = (_: Express) => {
+  return;
+};
---
+++ server/src/utils/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  port: 4000,
+  host: "localhost",
+  mongoURI: "mongodb://localhost:27017/codebook",
+  saltWorkFactor: 9,
+};
