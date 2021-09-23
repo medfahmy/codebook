815ce14b1b5cac043b7e80971fd345d3e3d3eb70
medfahmy <medfahmy99@gmail.com>
2021-09-23 11:16:34 +0100

    moved hashing to controller

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -5,100 +5,143 @@ import { omit } from "lodash";
 import bcryptjs from "bcryptjs";
 import { log } from "../config/logger";
 import { config } from "../config/config";
+import { signJWT } from "../utils/signJWT";
 
 export const getAllUsers = (
-  _req: Request,
-  res: Response,
-  _next: NextFunction
+    _req: Request,
+    res: Response,
+    _next: NextFunction
 ) => {
-  User.find()
-    .exec()
-    .then((result) => {
-      return res.status(200).json({
-        users: result,
-        count: result.length,
-      });
-    })
-    .catch((error) => {
-      return res.status(500).json({
-        message: error.message,
-        error,
-      });
-    });
+    User.find()
+        .select("-password")
+        .exec()
+        .then((users) => {
+            return res.status(200).json({
+                users,
+                count: users.length,
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: error.message,
+                error,
+            });
+        });
 };
 
 export const deleteAllUsers = (
-  _req: Request,
-  res: Response,
-  _next: NextFunction
+    _req: Request,
+    res: Response,
+    _next: NextFunction
 ) => {
-  User.deleteMany({})
-    .then(() => {
-      return res.status(200).send("success");
-    })
-    .catch((error) => {
-      return res.status(500).json({
-        message: error.message,
-        error,
-      });
-    });
+    User.deleteMany({})
+        .then(() => {
+            return res.status(200).json({
+                message: "success",
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: error.message,
+                error,
+            });
+        });
 };
 
 export const validateToken = (
-  req: Request,
-  res: Response,
-  _next: NextFunction
+    _req: Request,
+    res: Response,
+    _next: NextFunction
 ) => {
-  log.info("token validated");
-  return res.status(200).json({
-    message: "authorized",
-  });
+    log.info("token validated");
+    return res.status(200).json({
+        message: "authorized",
+    });
 };
 
-export const handleRegister = async (
-  req: Request,
-  res: Response,
-  _next: NextFunction
+export const handleRegister = (
+    req: Request,
+    res: Response,
+    _next: NextFunction
 ) => {
-  const { email, username, password } = req.body;
+    const { email, username, password } = req.body;
 
-  bcryptjs.hash(password, config.saltWorkFactor, (hashError, hash) => {
-    if (hashError) {
-      return res.status(500).json({
-        message: hashError.message,
-        error: hashError,
-      });
-    }
+    bcryptjs.hash(password, config.saltWorkFactor, (hashError, hash) => {
+        if (hashError) {
+            return res.status(500).json({
+                message: hashError.message,
+                error: hashError,
+            });
+        }
 
-    // TODO: insert user into db
-  });
+        const _user = new User({
+            _id: new Types.ObjectId(),
+            email,
+            username,
+            password: hash,
+        });
 
-  const user = new User({
-    _id: new Types.ObjectId(),
-    email,
-    username,
-    password,
-  });
-
-  return user
-    .save()
-    .then((result) => {
-      return res.status(201).json({
-        user: omit(result.toJSON(), "password"),
-      });
-    })
-    .catch((error) => {
-      return res.status(500).json({
-        message: error.message,
-        error,
-      });
+        return _user
+            .save()
+            .then((user) => {
+                return res.status(201).json(omit(user.toJSON(), "password"));
+            })
+            .catch((error) => {
+                return res.status(500).json({
+                    message: error.message,
+                    error,
+                });
+            });
     });
 };
 
 export const handleLogin = async (
-  req: Request,
-  res: Response,
-  _next: NextFunction
+    req: Request,
+    res: Response,
+    _next: NextFunction
 ) => {
-  const { email, username, password } = req.body;
+    const { email, password } = req.body;
+
+    User.find({ email })
+        .exec()
+        .then((users) => {
+            if (users.length !== 1) {
+                return res.status(401).json({
+                    message: "unauthorized",
+                });
+            }
+
+            bcryptjs.compare(password, users[0].password, (error, result) => {
+                if (error) {
+                    log.error(error.message, error);
+
+                    return res.status(401).json({
+                        message: "unauthorized",
+                    });
+                } else if (result) {
+                    signJWT(users[0], (_error, token) => {
+                        if (_error) {
+                            log.error("unable to sign token", error);
+
+                            return res.status(401).json({
+                                message: "unauthorized",
+                                error: _error,
+                            });
+                        } else if (token) {
+                            return res.status(200).json({
+                                message: "auth successful",
+                                token,
+                                user: omit(users[0].toJSON(), "password"),
+                            });
+                        }
+                    });
+                }
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: error.message,
+                error,
+            });
+        });
 };
