1eaf79145114513a6f9fbbfa225ff253cc1ff27c
medfahmy <medfahmy99@gmail.com>
2021-09-23 13:08:42 +0100

    await syntax

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import { log } from "./config/logger";
 import { logMethod } from "./middleware/logMethod";
 import mongoose from "mongoose";
 import cors from "cors";
+import { CellRouter } from "./routes/cell.router";
 
 const { port, host, mongoURI, mongoOptions } = config;
 
@@ -14,29 +15,34 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
+    cors({
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
 );
 app.use(logMethod);
 
 // routes
 app.use("/api/users", UserRouter);
+app.use("/api/cells", CellRouter);
 
 // database
-export const connectDB = () => {
-  return mongoose
-    .connect(mongoURI, mongoOptions)
-    .then(() => log.info("connected to mongodb"))
-    .catch((err) => {
-      log.error("mongodb error", err);
-      process.exit(1);
-    });
+export const connectDB = async () => {
+    return mongoose.connect(mongoURI, mongoOptions);
+    // .then(() => log.info("connected to mongodb"))
+    // .catch((err) => {
+    //   log.error("mongodb error", err);
+    //   process.exit(1);
+    // });
 };
 
 app.listen(port, host, () => {
-  log.info(`server running at http://${host}:${port}`);
-  connectDB();
+    log.info(`server running at http://${host}:${port}`);
+    connectDB()
+        .then(() => log.info("connected to mongodb"))
+        .catch((error) => {
+            log.error("mongodb error", error);
+            process.exit(1);
+        });
 });
