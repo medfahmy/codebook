90fdab72a5cf954fa74e9ba2ef9767b94de27114
medfahmy <medfahmy99@gmail.com>
2021-09-23 13:10:02 +0100

    refine

--- server/src/model/cell.model.ts
+++ server/src/model/cell.model.ts
@@ -3,20 +3,18 @@ import { Schema, model, Document } from "mongoose";
 export type CellType = "markdown" | "javascript";
 
 export interface CellDocument extends Document {
-  content: string;
-  type: CellType;
-  createdAt: Date;
-  updatedAt: Date;
+    content: string;
+    type: CellType;
 }
 
 const CellSchema = new Schema(
-  {
-    content: { type: String, required: true, unique: true },
-    type: { type: String, required: true },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        content: { type: String, required: true },
+        type: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
 );
 
 export const Cell = model<CellDocument>("Cell", CellSchema);
