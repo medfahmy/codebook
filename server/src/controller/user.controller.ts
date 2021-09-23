b5f1b7a02f0a8fabe8cec552a662878145992c15
medfahmy <medfahmy99@gmail.com>
2021-09-23 13:28:25 +0100

    await syntax

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -7,45 +7,41 @@ import { log } from "../config/logger";
 import { config } from "../config/config";
 import { signJWT } from "../utils/signJWT";
 
-export const getAllUsers = (
+export const getAllUsers = async (
     _req: Request,
     res: Response,
     _next: NextFunction
 ) => {
-    User.find()
-        .select("-password")
-        .exec()
-        .then((users) => {
-            return res.status(200).json({
-                users,
-                count: users.length,
-            });
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: error.message,
-                error,
-            });
+    try {
+        const users = await User.find().select("-password").exec();
+        return res.status(200).json({
+            users,
+            count: users.length,
         });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            error,
+        });
+    }
 };
 
-export const deleteAllUsers = (
+export const deleteAllUsers = async (
     _req: Request,
     res: Response,
     _next: NextFunction
 ) => {
-    User.deleteMany({})
-        .then(() => {
-            return res.status(200).json({
-                message: "success",
-            });
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: error.message,
-                error,
-            });
+    try {
+        await User.deleteMany({});
+        return res.status(200).json({
+            message: "success",
         });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            error,
+        });
+    }
 };
 
 export const validateToken = (
@@ -59,14 +55,14 @@ export const validateToken = (
     });
 };
 
-export const handleRegister = (
+export const handleRegister = async (
     req: Request,
     res: Response,
     _next: NextFunction
 ) => {
     const { email, username, password } = req.body;
 
-    bcryptjs.hash(password, config.saltWorkFactor, (hashError, hash) => {
+    bcryptjs.hash(password, config.saltWorkFactor, async (hashError, hash) => {
         if (hashError) {
             return res.status(500).json({
                 message: hashError.message,
@@ -81,67 +77,63 @@ export const handleRegister = (
             password: hash,
         });
 
-        return _user
-            .save()
-            .then((user) => {
-                return res.status(201).json(omit(user.toJSON(), "password"));
-            })
-            .catch((error) => {
-                return res.status(500).json({
-                    message: error.message,
-                    error,
-                });
+        try {
+            const user = await _user.save();
+            return res.status(201).json(omit(user.toJSON(), "password"));
+        } catch (error) {
+            return res.status(500).json({
+                message: error.message,
+                error,
             });
+        }
     });
 };
 
-export const handleLogin = (
+export const handleLogin = async (
     req: Request,
     res: Response,
     _next: NextFunction
 ) => {
     const { email, password } = req.body;
 
-    User.find({ email })
-        .exec()
-        .then((users) => {
-            if (users.length !== 1) {
+    try {
+        const users = await User.find({ email }).exec();
+        if (users.length !== 1) {
+            return res.status(401).json({
+                message: "unauthorized",
+            });
+        }
+
+        bcryptjs.compare(password, users[0].password, (error, result) => {
+            if (error) {
+                log.error(error.message, error);
+
                 return res.status(401).json({
                     message: "unauthorized",
                 });
+            } else if (result) {
+                signJWT(users[0], (_error, token) => {
+                    if (_error) {
+                        log.error("unable to sign token", error);
+
+                        return res.status(401).json({
+                            message: "unauthorized",
+                            error: _error,
+                        });
+                    } else if (token) {
+                        return res.status(200).json({
+                            message: "auth successful",
+                            token,
+                            user: omit(users[0].toJSON(), "password"),
+                        });
+                    }
+                });
             }
-
-            bcryptjs.compare(password, users[0].password, (error, result) => {
-                if (error) {
-                    log.error(error.message, error);
-
-                    return res.status(401).json({
-                        message: "unauthorized",
-                    });
-                } else if (result) {
-                    signJWT(users[0], (_error, token) => {
-                        if (_error) {
-                            log.error("unable to sign token", error);
-
-                            return res.status(401).json({
-                                message: "unauthorized",
-                                error: _error,
-                            });
-                        } else if (token) {
-                            return res.status(200).json({
-                                message: "auth successful",
-                                token,
-                                user: omit(users[0].toJSON(), "password"),
-                            });
-                        }
-                    });
-                }
-            });
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: error.message,
-                error,
-            });
         });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            error,
+        });
+    }
 };
